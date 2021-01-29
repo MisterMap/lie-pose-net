6b689c35258ec066fa14530d6c8c7a209676cc8c
Mikhail Kurenkov <Mikhail.Kurenkov@skoltech.ru>
2021-01-29 16:28:00 +0300

    #5: test POE SE3 loss

--- test/test_poe_se3_criterion.py
+++ test/test_poe_se3_criterion.py
@@ -10,10 +10,10 @@ class TestPOESE3Criterion(unittest.TestCase):
         torch.autograd.set_detect_anomaly(True)
         self._criterion = POESE3Criterion()
         self._position = (torch.arange(1, self._criterion.position_dimension + 1, requires_grad=True,
-                                       dtype=torch.float) * 0.3)[None]
+                                       dtype=torch.float, device="cuda:0") * 0.3)[None]
         self._position = torch.repeat_interleave(self._position, 2, dim=0)
         self._position[:6] = 0
-        self._target_position = torch.eye(4, 4)[None]
+        self._target_position = torch.eye(4, 4, device="cuda:0")[None]
         self._target_position = torch.repeat_interleave(self._target_position, 2, dim=0)
 
     def test_forward(self):
