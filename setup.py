a3949ffdb3b1ba0c6c051b0dc578ad53d39b28d1
Mikhail Kurenkov <kurenkov1995@mail.ru>
2021-02-28 16:53:34 +0300

    Add setup py

--- setup.py
+++ setup.py
@@ -10,6 +10,14 @@ if os.path.isfile(requirements_path):
     with open(requirements_path) as f:
         install_requires = f.read().splitlines()
 
+filtred_install_requires = []
+for require in install_requires:
+    if require[:2] == "-e":
+        print("Warning: you need install {} separatly".format(require))
+    else:
+        filtred_install_requires.append(require)
+install_requires = filtred_install_requires
+
 setup(name='lieposenet',
       version='0.1',
       description='LiePoseNet',
